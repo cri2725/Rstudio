6a58ae96cb684190f69d702e968c77222ee52790
cri2725 <cristian.istrati27@gmail.com>
2025-04-03 15:38:20 +0200

    primo es secondo file

--- primofile.R
+++ primofile.R
@@ -1,3 +1,6 @@
-a<-5
-a
-  
+#1. Definisci il vettore y con gli elementi 8, 3, 5, 7, 6, 6, 8, 9, 2.
+# Gli elementi di y sono minori di 5?
+# Crea un nuovo vettore z con gli elementi di y minori di 5.
+y<-c(8, 3, 5, 7, 6, 6, 8, 9, 2)
+z<-y[y<5]
+print(z)
